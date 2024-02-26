76221291be1cc95b19095896f274f09d9e8bd616
KospiPL <kacper.cudzik11@gmail.com>
2024-02-26 10:28:59 +0100

    Dodanie okna logowania skonczone okno głwne i pasek nawigacyjny

--- D.W.C.APP/MauiProgram.cs
+++ D.W.C.APP/MauiProgram.cs
@@ -1,6 +1,13 @@
-﻿
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Hosting;
+using D.W.C.APP; // Upewnij się, że namespace odpowiada nazwie Twojego namespace.
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Logging;
+using D.W.C.APP.Shared;
+using D.W.C.APP.Service;
+
 
 namespace D.W.C.APP
 {
@@ -10,22 +17,29 @@ namespace D.W.C.APP
         {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>()
+                .UseMauiApp<App>() 
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+
                 });
 
+            // Konfiguracja Blazor i usług lokalnych
             builder.Services.AddMauiBlazorWebView();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddBlazoredLocalStorage();
-
-
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
 #endif
 
+            // Dodanie lokalnego przechowywania danych
+            builder.Services.AddBlazoredLocalStorage();
+
+            // Konfiguracja systemu autoryzacji
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            // Logowanie - dostosuj poziom logowania w zależności od potrzeb
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
 
             return builder.Build();
         }
